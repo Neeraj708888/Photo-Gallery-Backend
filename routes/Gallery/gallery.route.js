57a1f18bb6fd84b03cdd61964e876e8ce0703608
Neeraj Kumar <amck.sharma@gmail.com>
2026-01-17 23:40:37 +0530

    Veridy Token is implemented

--- routes/Gallery/gallery.route.js
+++ routes/Gallery/gallery.route.js
@@ -1,15 +1,16 @@
 import express from "express";
 import { createGallery, deleteGallery, getAllGallery, getSingleGallery, toggleGalleryStatus, updateGallery } from "../../controllers/Gallery/gallery.controller.js";
 import { upload } from "../../middlewares/cloudinary.js";
+import { verifyToken } from "../../middlewares/auth.middlewares.js";
 
 const router = express.Router();
 
 
 // router.get("/search", filterGallery);
-router.post("/create", upload.single("thumbnail"), createGallery);
-router.post("/update/:id", upload.single("thumbnail"), updateGallery);
-router.patch("/status/:id", toggleGalleryStatus);
-router.delete("/:id", deleteGallery);
+router.post("/create", verifyToken, upload.single("thumbnail"), createGallery);
+router.post("/update/:id", verifyToken, upload.single("thumbnail"), updateGallery);
+router.patch("/status/:id", verifyToken, toggleGalleryStatus);
+router.delete("/:id", verifyToken, deleteGallery);
 router.get("/", getAllGallery);
 router.get("/:id", getSingleGallery);
 
